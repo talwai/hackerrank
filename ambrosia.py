e13d2fa66a55787b5dac0e5cd50b7fe9f170d1d7
Aaditya Talwai <talwai@dartmouth.edu>
2013-08-13 01:52:51 -0400

    revamped optimal structure

--- ambrosia.py
+++ ambrosia.py
@@ -9,15 +9,6 @@ manage = [[0 for x in xrange(NUM_CITIES)] for x in xrange(NUM_CITIES)]
 
 UNDEFINED = 1000000
 min_cost = [[UNDEFINED for x in xrange(NUM_CITIES)] for x in xrange(NUM_CITIES)]
-managed = []
-
-
-class City:
-    def __init__ (self, id):
-        self.id = id
-        self.managed_by = -1
-        self.in_road = -1
-        self.out_road = -1
 
 ALL_CITIES = [x for x in xrange(NUM_CITIES)]
 
@@ -33,35 +24,66 @@ for i in range(1+NUM_CITIES,2*NUM_CITIES+1):
     for j in range(0,len(tokens)):
         manage [i-NUM_CITIES-1][j] = int(tokens[j])
 
-#for i in ALL_CITIES:
-	#construct[i][i] = UNDEFINED
-
-def total_cost(i,j,k):
+def total_cost(i,j, k):
     return construct[i][j] + manage[i][k]
 
-def get_min_construct(i, path):
+def get_min_construct(i, path = []):
     return construct[i].index(min(construct[i][k] for k in ALL_CITIES if k not in path))
 
-def get_min_manager(i, managers):
-    return manage[i].index(min(manage[i][x] for x in ALL_CITIES if x not in managers))
+def get_min_manager(i, managers = []):
+    print "MANAGERS ARE ", managers
+    ret = manage[i].index(min(manage[i][x] for x in ALL_CITIES if x not in managers))
+    print "Min manager for city ", i , "is ", ret
+    return ret
+
+def get_optimal_managers(i, managers = [], cities = ALL_CITIES):
+    optimal_managers = [UNDEFINED for x in ALL_CITIES]
+    j = get_min_manager(i, managers)
+    optimal_managers[i] = j
+    managers.append(j)
+    for j in cities:
+        if j != i:
+            x = get_min_manager(j, managers)
+            optimal_managers[j] = x
+            managers.append(x)
+
+    total_sum = 0
+
+    max_man = max(manage[x][optimal_managers[x]] for x in cities)
+
+    for x in cities:
+        total_sum = total_sum + manage[x][optimal_managers[x]]
+    print "TOTAL SUM" , total_sum, optimal_managers
+
+    return total_sum , optimal_managers
+
+def optimal_manage_cost(path):
+    print path[:-1]
+    total_sum, optimal_managers = min(get_optimal_managers(i, [], cities = path[:-1]) for i in path[:-1])
+    return total_sum
+
+OPTIMAL_COSTS = []
 
 def min_path(i, path, managers):
     if len(path) == 0:
     	path.append(i)
     elif len(path) == NUM_CITIES:
-        print path
-        print managers
+        OPTIMAL_COSTS.append(optimal_manage_cost(path))
         return 0
     j = get_min_construct(i, path)
-    k = get_min_manager(i, managers)
-    managers.append(k)
     path.append(j)
-    return total_cost(i, j, k) + min_path(j,path,managers)
+    return construct[i][j] + min_path(j,path,managers)
 
 def global_min():
-    return min(min_path(city, [],[]) for city in ALL_CITIES)
+    curr_min = UNDEFINED
+    optimal_cost = UNDEFINED
 
-
+    all_costs = [min_path(city,[],[]) for city in ALL_CITIES]
+    for i in xrange(len(all_costs)):
+    	optimal_cost = OPTIMAL_COSTS[i] + all_costs[i]
+        if optimal_cost < curr_min:
+        	curr_min = optimal_cost
+    return curr_min
 
 print global_min()
 
